fcfb876a28b974f508d4520213e9d6509f4c3c27
Brendan Long <self@brendanlong.com>
2014-05-20 14:31:20 -0500

    Switch to Python3, remove dependency on feedparser, and make style more closely follow PEP8.

--- inliner.py
+++ inliner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vim:tabstop=4:expandtab:sw=4:softtabstop=4
 
@@ -6,17 +7,18 @@
 #       check python-cssutils, if it's of any use. for now it's a naive regexp
 #
 
-import sys,re,urllib2,base64,mimetypes,urlparse
-from BeautifulSoup import BeautifulSoup
-
-#feedparse has a _kick ass_ charset detection function!
-import feedparser
+import base64
+import mimetypes
+import re
+import urllib
+import sys
+from bs4 import BeautifulSoup
 
 def is_remote(address):
-    return urlparse.urlparse(address)[0] in ('http','https')
+    return urllib.parse.urlparse(address)[0] in ('http', 'https')
 
-def data_encode_image(name,content):
-    return u'data:%s;base64,%s' % (mimetypes.guess_type(name)[0],base64.standard_b64encode(content))
+def data_encode_image(name, content):
+    return 'data:%s;base64,%s' % (mimetypes.guess_type(name)[0], base64.standard_b64encode(content).decode("utf-8"))
 
 def ignore_url(address):
     url_blacklist = ('getsatisfaction.com',
@@ -28,40 +30,32 @@ def ignore_url(address):
 
     return False
 
-def get_content(from_,expect_binary=False):
-#{{
+def get_content(from_, expect_binary=False):
     if is_remote(from_):
         if ignore_url(from_):
-            return u''
+            return ''
 
-        ct = urllib2.urlopen(from_)
-        if not expect_binary:
-            s = ct.read()
-            encodings = feedparser._getCharacterEncoding(ct.headers,s)
-            return unicode(s,encodings[0])
+        with urllib.request.urlopen(from_) as f:
+            data = f.read()
+        if expect_binary:
+            return data
         else:
-            return ct.read()
+            return data.decode("utf-8")
     else:
-        s = open(from_).read()
-        if not expect_binary:
-            encodings = feedparser._getCharacterEncoding({},s)
-            return unicode(s,encodings[0])
-        else:
-            return s
-#}}
+        with open(from_, "rb" if expect_binary else "r") as f:
+            return f.read()
 
-def resolve_path(base,target):
-#{{
+def resolve_path(base, target):
     if True:
-        return urlparse.urljoin(base,target)
+        return urllib.parse.urljoin(base, target)
 
     if is_remote(target):
         return target
 
     if target.startswith('/'):
         if is_remote(base):
-            protocol,rest = base.split('://')
-            return '%s://%s%s' % (protocol,rest.split('/')[0],target)
+            protocol, rest = base.split('://')
+            return '%s://%s%s' % (protocol, rest.split('/')[0], target)
         else:
             return target
     else:
@@ -70,71 +64,64 @@ def resolve_path(base,target):
             return '%s/%s' % (base, target)
         except ValueError:
             return target
-#}}
 
-def replaceJavascript(base_url,soup):
-#{{
-    for js in soup.findAll('script',{'src':re.compile('.+')}):
+def replace_javascript(base_url, soup):
+    for js in soup.find_all('script', {'src': re.compile('.+')}):
         try:
-            real_js = get_content(resolve_path(base_url,js['src']))
-            js.replaceWith(u'<script>%s</script>' % real_js)
-        except Exception,e:
-            print 'failed to load javascript from %s' % js['src']
-            print e
-            #js.replaceWith('<!-- failed to load javascript from %s -->' % js['src'])
-#}}
+            real_js = get_content(resolve_path(base_url, js['src']))
+            new_tag = soup.new_tag('script')
+            new_tag.string = real_js
+            js.replace_with(new_tag)
+        except Exception as e:
+            print('failed to load javascript from %s' % js['src'])
+            print(e)
 
-css_url = re.compile(ur'url\((.+)\)')
-def replaceCss(base_url,soup):
-#{{
-    for css in soup.findAll('link',{'rel':'stylesheet','href':re.compile('.+')}):
+css_url = re.compile(r'url\((.+)\)')
+def replace_css(base_url, soup):
+    for css in soup.findAll('link', {'rel': 'stylesheet', 'href': re.compile('.+')}):
         try:
-            real_css = get_content(resolve_path(base_url,css['href']))
+            real_css = get_content(resolve_path(base_url, css['href']))
 
             def replacer(result):
                 try:
-                    path = resolve_path(resolve_path(base_url,css['href']),result.groups()[0])
-                    return u'url(%s)' % data_encode_image(path,get_content(path,True))
-                except Exception,e:
-                    print e
-                    return u''
+                    path = resolve_path(resolve_path(base_url, css['href']), result.groups()[0])
+                    return 'url(%s)' % data_encode_image(path, get_content(path, True))
+                except Exception as e:
+                    print('failed to encode css')
+                    print(e)
+                    return ''
 
-            css.replaceWith(u'<style>%s</style>' % re.sub(css_url,replacer,real_css))
+            new_tag = soup.new_tag('style')
+            new_tag.string = re.sub(css_url, replacer, real_css)
+            css.replaceWith(new_tag)
 
-        except Exception,e:
-            print 'failed to load css from %s' % css['href']
-            print e
-            #css.replaceWith('<!-- failed to load css from %s -->' % css['href'])
-#}}
+        except Exception as e:
+            print('failed to load css from %s' % css['href'])
+            print(e)
 
-def replaceImages(base_url,soup):
-#{{
+def replace_images(base_url,soup):
     from itertools import chain
 
-    for img in chain(soup.findAll('img',{'src':re.compile('.+')}),
-                     soup.findAll('input',{'type':'image','src':re.compile('.+')})):
+    for img in chain(soup.findAll('img', {'src': re.compile('.+')}),
+                     soup.findAll('input', {'type': 'image', 'src': re.compile('.+')})):
         try:
-            path = resolve_path(base_url,img['src'])
-            real_img = get_content(path,True)
-            img['src'] = data_encode_image(path.lower(),real_img)
-        except Exception,e:
-            print 'failed to load image from %s' % img['src']
-            print e
-            #img.replaceWith('<!-- failed to load image from %s -->' % img['src'])
-
-#}}
+            path = resolve_path(base_url, img['src'])
+            real_img = get_content(path, True)
+            img['src'] = data_encode_image(path.lower(), real_img)
+        except Exception as e:
+            print('failed to load image from %s' % img['src'])
+            print(e)
 
 def main(url,output_filename):
-    bs = BeautifulSoup(get_content(url))
+    soup = BeautifulSoup(get_content(url))
 
-    replaceJavascript(url,bs)
-    replaceCss(url,bs)
-    replaceImages(url,bs)
+    replace_javascript(url, soup)
+    replace_css(url, soup)
+    replace_images(url, soup)
 
-    res = open(output_filename,'wb')
-    print >>res,str(bs)
-    res.close()
+    with open(output_filename,'wb') as res:
+        res.write(str(soup).encode("utf-8"))
 
 if __name__ == '__main__':
-    main(sys.argv[1],sys.argv[2])
+    main(sys.argv[1], sys.argv[2])
 
